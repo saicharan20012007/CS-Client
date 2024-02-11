1fee6d3111f1af2ae0eb7ac16ff3104c12b71326
Sai Charan <saicharan20012007@gmail.com>
2024-02-11 07:33:50 +0530

    auth

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
-  const { accessToken } = useSelector((store) => store.AuthReducer);
+  const accessToken = localStorage.getItem("accessToken.geometry");
 
   console.log(accessToken);
 
